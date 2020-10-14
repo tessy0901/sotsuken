8a1b2cd26f834a7086fcaf4ef3f6fd5b651522ad
tessy0901 <tessy0901@gmail.com>
2020-10-14 10:40:16 +0900

    中間発表

--- yobi/public/script2.js
+++ yobi/public/script2.js
@@ -2,23 +2,23 @@
 const analytics = firebase.analytics();
 
 const num = 77;
-const answer = 32
+const answer = 19
 
 for (let i = 0; i < num; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
     if(i === answer) {
-        div.textContent = "侮"
+        div.textContent = "巳"
         div.classList.add("success")
     } else {
-        div.textContent = "悔"
+        div.textContent = "已"
         div.classList.add("fail")
     }
 
     div.addEventListener('click', () => {
         if(i === answer) {
             div.textContent = "正解"
-            window.location.href = 'second.html';
+            window.location.href = 'therd.html';
             //analytics.logEvent('select_content');
         } else {
             div.textContent = "不正解"
--- yobi/public/script3.js
+++ yobi/public/script3.js
@@ -2,24 +2,26 @@
 const analytics = firebase.analytics();
 
 const num = 77;
-const answer = 32
+const answer = 55
+//const startDate = Date.now()
 
 for (let i = 0; i < num; i++) {
     const div = document.createElement('div');
     div.classList.add('box');
     if(i === answer) {
-        div.textContent = "侮"
+        div.textContent = "候"
         div.classList.add("success")
     } else {
-        div.textContent = "悔"
+        div.textContent = "侯"
         div.classList.add("fail")
     }
 
     div.addEventListener('click', () => {
         if(i === answer) {
             div.textContent = "正解"
-            window.location.href = 'second.html';
-            //analytics.logEvent('select_content');
+            const endDate = Date.now()
+            //const selectDate = endDate - startDate
+            //analytics.logEvent('select_content3', selectDate);
         } else {
             div.textContent = "不正解"
         }
